3eb40095c5d087ecb7d16e70ce51108b032e07b3
PabloCofre850 <pablo.cofre@alumnos.ucm.cl>
2025-11-05 11:18:10 -0300

    Clases kotlincmp

--- composeApp/src/commonMain/kotlin/com/example/evaluacion2/shared/dominio/Boleta.kt
+++ composeApp/src/commonMain/kotlin/com/example/evaluacion2/shared/dominio/Boleta.kt
@@ -1,8 +1,11 @@
 package com.example.evaluacion2.shared.dominio
 
 // Importamos esto para poder convertir la clase a JSON fácilmente
+import kotlin.math.pow
 
 import kotlinx.serialization.Serializable
+
+
 
 // La clase Boleta representa una boleta de consumo eléctrico emitida a un cliente.
 // Es una 'data class' porque solo guarda información (no tiene lógica).
@@ -15,4 +18,37 @@ data class Boleta(
     val kwhTotal: Double,
     val detalle: TarifaDetalle,
     val estado: EstadoBoleta
-)
+): ExportablePDF {
+
+    // Implementación de la función para exportar la boleta como tabla PDF
+    override fun toPdfTable(): PdfTable {
+
+        val headers = listOf("Campo", "Valor")
+
+        // Función auxiliar para redondear y convertir Double a String con seguridad
+        //ya que no nos dejo utiizar String.format o .format
+
+        fun Double.toStringSafe(decimals: Int = 2): String {
+            val factor = 10.0.pow(decimals)
+            val rounded = kotlin.math.round(this * factor) / factor
+            return rounded.toString()
+        }
+
+        val rows = listOf(
+            listOf("Cliente", idCliente),
+            listOf("Año", anio.toString()),
+            listOf("Mes", mes.toString()),
+            listOf("kWh Total", kwhTotal.toStringSafe()),
+            listOf("Tarifa", detalle.nombre),
+            listOf("Precio kWh", detalle.precioKwh.toStringSafe()),
+            listOf("Consumo", detalle.consumo.toStringSafe()),
+            listOf("Total", detalle.total.toStringSafe()),
+            listOf("Estado", estado.name)
+        )
+
+        return PdfTable(headers, rows)
+        }
+
+
+
+    }
--- composeApp/src/commonMain/kotlin/com/example/evaluacion2/shared/dominio/PdfTable.kt
+++ composeApp/src/commonMain/kotlin/com/example/evaluacion2/shared/dominio/PdfTable.kt
@@ -2,5 +2,5 @@ package com.example.evaluacion2.shared.dominio
 
 class PdfTable (
     var headers: List<String>,
-    var rows: List<List<List<String>>>,
+    var rows: List<List<String>>,
 )
