77d958f21486cb2aad2d24d3a75bdf2ba0f3d6a3
Ignacio Vasquez <ignaciojav9@gmail.com>
2025-11-05 12:22:37 -0300

    pantalla boletas comentado

--- composeApp/src/commonMain/kotlin/com/example/evaluacion2/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/evaluacion2/App.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.evaluacion2.shared.vistas.BoletaTestScreen
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -26,7 +25,6 @@ fun App() {
     @Composable
     fun App() {
         MaterialTheme {
-           BoletaTestScreen()
         }
     }
 
