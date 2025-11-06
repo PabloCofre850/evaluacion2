a17b45ed398226733a9cb6a8c618029b5bb29442
Ignacio Vasquez <ignaciojav9@gmail.com>
2025-11-05 23:03:01 -0300

    avance numero xd

--- composeApp/src/commonMain/kotlin/com/example/evaluacion2/ui/screens/PantallaMenu.kt
+++ composeApp/src/commonMain/kotlin/com/example/evaluacion2/ui/screens/PantallaMenu.kt
@@ -31,7 +31,8 @@ fun PantallaMenu(
             verticalArrangement = Arrangement.Center
         ) {
             Text("¡Bienvenido!", style = MaterialTheme.typography.displayLarge)
-            Text("CGE", style = MaterialTheme.typography.headlineSmall)
+            Text("CGE", style = MaterialTheme.typography.displayLarge)
+            Text("Tu empresa de confianza!", style = MaterialTheme.typography.displayLarge)
         }
 
         Spacer(Modifier.width(40.dp))
